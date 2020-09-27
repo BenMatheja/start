66c1ed58d7a45fd0bbc2d53fb8b502d81efab350
Ben Matheja <post@benmatheja.de>
2020-09-27 12:23:36 +0200

    prototype for portal

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,12 +3,6 @@ import { css } from "@emotion/core"
 import { rhythm } from "../utils/typography"
 import { useStaticQuery, Link, graphql } from "gatsby"
 
-const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem`}}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
-
 export default function Layout({ children }) {
   const data = useStaticQuery(
     graphql`
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,35 @@ export default function Home() {
         What do I like to do? Lots of course but definitely enjoy building
         websites.
       </p>
+      <ul>
+        <li>
+          <a href="https://git.quaysi.de">git</a>
+        </li>
+        <li>
+          <a href="https://drone.quaysi.de">drone</a>
+        </li>
+        <li>
+          <a href="https://pihole.quaysi.de/admin">pihole</a>
+        </li>
+        <li>
+          <a href="https://unifi.quaysi.de">unifi</a>
+        </li>
+        <li>
+          <a href="https://nas.quaysi.de">nas</a>
+        </li>
+        <li>
+          <a href="https://news.ycombinator.com">hacker news</a>
+        </li>
+        <li>
+          <a href="https://www.github.com">github</a>
+        </li>
+        <li>
+          <a href="http://twitter.com/">twitter</a>
+        </li>
+        <li>
+          <a href="http://twitch.tv/">twitch</a>
+        </li>
+      </ul>
     </Layout>
   );
 }
