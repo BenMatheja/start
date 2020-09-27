b2dfdc2003cfdd5f9f41e89a48a6317ad0f05cf7
Ben Matheja <post@benmatheja.de>
2020-09-27 12:07:34 +0200

    added graphql data retrieval

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Quaysi.de`,
+  },
   plugins: [
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
